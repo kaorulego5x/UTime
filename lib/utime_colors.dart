ff56f61d688a564a4390fc2b69871cebdd4916f7
Mint-ch <mint15ch@gmail.com>
2022-03-30 02:50:49 +0900

    ドロップダウンのカラーを設定

--- lib/utime_colors.dart
+++ lib/utime_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class UtimeColors {
   //背景
-  static const backgroundColor = Color(0xFFE4F9FB);
+  static const backgroundColor = Color(0xFFE6F8FC);
 
   //タブのアクセントカラー
   static const tabAccent = Color(0xFFED6969);
@@ -24,20 +24,28 @@ class UtimeColors {
   //科目区分
   //必修
   static const subject1 = Color(0xFFB1E3FF);
+  static const darkSubject1 = Color(0xFFA3D4EF);
   //L系列
-  static const subject2 = Color(0xFFFFF1A7);
+  static const subject2 = Color(0xFFFFF1A5);
+  static const darkSubject2 = Color(0xFFF8E68A);
   //理系A~D、文系A~C
-  static const subject3 = Color(0xFFFFB1B5);
+  static const subject3 = Color(0xFFFFD7E8);
+  static const darkSubject3 = Color(0xFFF3C2D7);
   //文系:社会科学、人文科学
-  static const subject4 = Color(0xFFFFD3E0);
+  static const subject4 = Color(0xFFFFB1B5);
+  static const darkSubject4 = Color(0xFFF29CA0);
   //理系E~F、文系D~F
-  static const subject5 = Color(0xFFE3CEF4);
+  static const subject5 = Color(0xFFE4DAFF);
+  static const darkSubject5 = Color(0xFFD1C5F1);
   //集中講義
-  static const subject6 = Color(0xFFC9F5AD);
+  static const subject6 = Color(0xFFCDEFC1);
+  static const darkSubject6 = Color(0xFFB5DEA7);
   //未定（グレー）
   static const subject7 = Color(0xffd9d9d9);
+  static const darkSubject7 = Color(0xFFC8C8C8);
   //展開科目
-  static const subject8 = Color(0xFFFFD29D);
+  static const subject8 = Color(0xFFFFDAAE);
+  static const darkSubject8 = Color(0xFFF8CB96);
 
   //IntensiveCourseAreaの追加アイコンの色
   static const intensiveAdd = Color(0xffd9d9d9);
@@ -47,21 +55,21 @@ class UtimeColors {
   ///dialogColorに応じてdropButtonColorを返す(途中)
   Color getDropDownColor(Color dialogColor) {
     if (dialogColor == UtimeColors.subject1) {
-      return UtimeColors.subject1;
+      return UtimeColors.darkSubject1;
     } else if (dialogColor == UtimeColors.subject2) {
-      return UtimeColors.subject2;
+      return UtimeColors.darkSubject2;
     } else if (dialogColor == UtimeColors.subject3) {
-      return UtimeColors.subject3;
+      return UtimeColors.darkSubject3;
     } else if (dialogColor == UtimeColors.subject4) {
-      return UtimeColors.subject4;
+      return UtimeColors.darkSubject4;
     } else if (dialogColor == UtimeColors.subject5) {
-      return UtimeColors.subject5;
+      return UtimeColors.darkSubject5;
     } else if (dialogColor == UtimeColors.subject6) {
-      return UtimeColors.subject6;
+      return UtimeColors.darkSubject6;
     } else if (dialogColor == UtimeColors.subject7) {
-      return UtimeColors.subject7;
+      return UtimeColors.darkSubject7;
     } else if (dialogColor == UtimeColors.subject8) {
-      return UtimeColors.subject8;
+      return UtimeColors.darkSubject8;
     } else {
       throw Exception('dialogcolor is $dialogColor');
     }
